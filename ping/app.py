3ee47a836299362190f70fbafe0c29779e51ca3c
shinkeonkim <singun11@kookmin.ac.kr>
2024-08-11 21:05:24 +0900

    refactor: ping-app, ping-service

--- ping/app.py
+++ ping/app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,4 +7,4 @@ def hello_world():
     return jsonify(message="Hello, World!")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5555)
+    app.run(host='0.0.0.0', port=5550)
